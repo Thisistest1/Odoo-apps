dfd36b45d8e0592f4f25dd811c4e4987680084e0
Faslu <faslu@cybrosys.in>
2017-10-24 16:06:22 +0530

    Initial Commit

---
+++ test/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Test',
+    'summary': """This is a test""",
+    'version': '10.0.1.0',
+    'description': """This is a test""",
+    'author': 'Test User',
+    'company': '',
+    'website': '',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': '',
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
